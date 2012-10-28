8ab4ad02efb3882ef784536e59cc9093969707ec
Anuj Arora <anuj.arora1@gmail.com>
2012-10-28 14:42:06 -0400

    removed unsed imports

--- src/summarizer/HtmlParser.java
+++ src/summarizer/HtmlParser.java
@@ -1,16 +1,13 @@
 package summarizer;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import cache.DocumentCache;
 
